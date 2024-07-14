69f3b50210934c96b996df38c2b03280bb72689e
Ali Alrefaie <alrefaiea6@gmail.com>
2024-07-13 20:15:17 -0400

    Created tasks.js file

---
+++ my-app/src/tasks.js
@@ -0,0 +1,5 @@
+export const tasks = [
+    { id: 1, name: "Complete the React challenge", isCompleted: false },
+    { id: 2, name: "Track progress and documentation", isCompleted: false },
+    { id: 3, name: "Prepare presentation", isCompleted: false }
+];
